73d2a329dd75ead0a785e91ddf563e9973acba3f
Suneel Kumar <sunilkumar05644@gmail.com>
2022-07-28 17:15:35 +0530

    first commit

---
+++ REAME.md
@@ -0,0 +1,25 @@
+# Vimeo downloder
+
+this is vimeo downloder libery
+
+## Getting Started
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+```
+npm i vimeo
+```
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+Say what the step will be
+
+```
+const vimeo = require('vimeo');
+
+const data =  vimeo("https://vimeo.com/731865515")
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+
+function getIdFromVimeoURL(url) {
+    return /(vimeo(pro)?\.com)\/(?:[^\d]+)?(\d+)\??(.*)?$/.exec(url)[3];
+  }
+
+async function vimeo(url) {
+    const axios = require("axios"); // Importing the Axios module to make API requests
+    const videoID =  getIdFromVimeoURL(url)
+    finalUrl = `https://player.vimeo.com/video/${videoID}/config`
+    const getData = await axios.get(finalUrl);
+    const dataUrl = getData.data.request.files.progressive[0].url;
+      const duration = await getVideoDurationInSeconds(dataUrl);
+     const finalObj = {
+        contentLength: duration,
+        videoUrl: dataUrl,
+        videoId: getData.data.video.id,
+        title: getData.data.video.title,
+        thumbnail: getData.data.seo.thumbnail,
+      };
+    //   res
+    //   .status(200)
+    //   .send({ msg: "video downloaded url get", response: finalObj });
+    return finalObj;
+  };
+module.exports = vimeo
